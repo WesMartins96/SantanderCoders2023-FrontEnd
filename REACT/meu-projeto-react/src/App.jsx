0d03e08a383673c4a97fc9dedd5705bbd03068c8
Wesley Martins <wesley.mlopes@castgroup.com>
2023-05-29 03:39:25 -0300

    att

--- REACT/meu-projeto-react/src/App.jsx
+++ REACT/meu-projeto-react/src/App.jsx
@@ -1,26 +1,17 @@
-import MeuComponente from "./components/MeuComponente"
+
 
 function App() {
   
 
   return (
     <div>
-      <h1>Olá Mundo, React!</h1>
-      <MeuComponente/>
-      <MeuBotao conteudo='Salvar'/>
-      <MeuBotao conteudo='Voltar'/>
-      <MeuBotao conteudo='Deletar'/>
+      <h1>Estados e Eventos</h1>
     </div>
   )
 }
 
 
 
-function MeuBotao(props){
-  return(
-    <button>{props.conteudo}</button>
-  )
-}
 
 
 export default App
